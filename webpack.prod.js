9f3b645db77cd1ef8525047ae1adedffc0b1e37e
tmiyachi <t.miyachi@hotmail.co.jp>
2021-03-06 16:25:33 +0900

    update webpack5

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    minimize: true,
+    minimizer: [new TerserJSPlugin(), new CssMinimizerPlugin()],
   },
 });
