88d8442be785bde8ac2f742f103f8ca12e45e5c7
tmiyachi <t.miyachi@hotmail.co.jp>
2021-03-13 15:05:14 +0900

    デフォルト開発環境をES2020にする

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2016,
+    ecmaVersion: 2020,
   },
   rules: {},
 };
