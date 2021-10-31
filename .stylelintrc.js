2040db3c821e46cca79d01659d11966b5918c1b7
tmiyachi <t.miyachi@hotmail.co.jp>
2021-10-31 14:21:16 +0900

    update stylelint 14.x

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,5 +4,11 @@ module.exports = {
     'stylelint-config-recess-order',
     'stylelint-prettier/recommended',
   ],
+  overrides: [
+    {
+      files: ['**/*.scss'],
+      customSyntax: 'postcss-scss',
+    },
+  ],
   rules: {}
 };
