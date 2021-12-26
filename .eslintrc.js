6cd1a442aca0db72f519ae87df89e3bacd292053
tmiyachi <t.miyachi@hotmail.co.jp>
2021-12-26 20:07:13 +0900

    ESlintの重複する設定を削除
    `env: es2020: true` の場合は ecmaVersion が自動的に 11(=2020) となるため

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
+/* eslint-env node */
 module.exports = {
   env: {
     browser: true,
-    es6: true,
-    node: true,
+    es2020: true,
   },
   extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2020,
   },
   rules: {},
 };
