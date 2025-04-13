32ffbdcb044f9df5d4611c0268746d89bf46af15
tmiyachi <t.miyachi@hotmail.co.jp>
2025-04-13 17:36:59 +0900

    モードを設定で参照できるようにする

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,57 +10,59 @@ const BANNER = `
 /*! FOLLOWING LIBRARIES ARE USED.
  */`;
 
-export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  // base: './', // ルートではないパブリックパスにデプロイする場合
-  publicDir: path.resolve(__dirname, 'public'),
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-
-  build: {
-    // target: ['firefox86'], // バンドルのブラウザ互換性のターゲットを指定する
-    outDir: '../dist',
-    assetsDir: 'assets',
-    // assetsInlineLimit: 0,  // アセットのをbase64インライン化を無効にする場合
-    // sourcemap: true,
-    emptyOutDir: true, // ビルド時に outDir を空にする
-
-    // assetsInclude
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].bundle.js',
-        chunkFileNames: `js/[name].js`, // vendor chunk filenames
-        assetFileNames: `assets/[name].[ext]`, // CSSもassetsになる
+export default defineConfig(({ mode }) => {
+  return {
+    root: path.resolve(__dirname, 'src'),
+    // base: './', // ルートではないパブリックパスにデプロイする場合
+    publicDir: path.resolve(__dirname, 'public'),
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
-      plugins: [
-        license({
-          sourcemap: true,
-          banner: BANNER,
-          thirdParty: {
-            // includePrivate: true, // Default is false.
-            // multipleVersions: true, // Default is false.
-            // output: {
-            //   file: path.join(__dirname, 'dist', 'DEPENDENCIES.txt'),
-            // },
-          },
-        }),
-      ],
     },
-  },
 
-  server: {
-    port: 8080,
-    open: true,
-    proxy: {
-      // '/api': 'http://localhost:8081',
-      // '/api': {
-      //  target: 'http://example.com',
-      //  changeOrigin: true,
-      //  rewrite: (path) => path.replace(/^\/api/, ''),
+    build: {
+      // target: ['firefox86'], // バンドルのブラウザ互換性のターゲットを指定する
+      outDir: '../dist',
+      assetsDir: 'assets',
+      // assetsInlineLimit: 0,  // アセットのをbase64インライン化を無効にする場合
+      // sourcemap: true,
+      emptyOutDir: true, // ビルド時に outDir を空にする
+
+      // assetsInclude
+      rollupOptions: {
+        output: {
+          entryFileNames: '[name].bundle.js',
+          chunkFileNames: `js/[name].js`, // vendor chunk filenames
+          assetFileNames: `assets/[name].[ext]`, // CSSもassetsになる
+        },
+        plugins: [
+          license({
+            sourcemap: true,
+            banner: BANNER,
+            thirdParty: {
+              // includePrivate: true, // Default is false.
+              // multipleVersions: true, // Default is false.
+              // output: {
+              //   file: path.join(__dirname, 'dist', 'DEPENDENCIES.txt'),
+              // },
+            },
+          }),
+        ],
+      },
     },
-  },
+
+    server: {
+      port: 8080,
+      open: true,
+      proxy: {
+        // '/api': 'http://localhost:8081',
+        // '/api': {
+        //  target: 'http://example.com',
+        //  changeOrigin: true,
+        //  rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  };
 });
